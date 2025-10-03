86e7d4b818aa56c4b7e71db9fe226132cb5400a3
daniel.rodrigues <daniel.rodrigues@aegro.com.br>
2025-10-03 14:57:26 -0300

    Fix GitHub Pages blank page issue
    - Switch from BrowserRouter to HashRouter for better GitHub Pages compatibility
    - Add 404.html for SPA routing support on GitHub Pages
    - Update index.html with SPA routing script
    - Remove basename from Router since HashRouter handles it differently
    - Improve page title and meta description

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
@@ -8,7 +8,7 @@ import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <Router basename="/daniel-portfolio">
+    <Router>
       <div className="App">
         <Navbar />
         <main>
