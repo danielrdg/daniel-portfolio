ff779496f856a8f3e3c981b84c8531d9db4edbd4
daniel.rodrigues <daniel.rodrigues@aegro.com.br>
2025-10-03 14:38:10 -0300

    Initial commit: React portfolio project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { Link, useLocation } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+  const location = useLocation()
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="nav-content">
+          <Link to="/" className="nav-logo">
+            <span>Portfolio</span>
+          </Link>
+          
+          <div className="nav-links">
+            <Link 
+              to="/" 
+              className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}
+            >
+              Home
+            </Link>
+            <Link 
+              to="/about" 
+              className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`}
+            >
+              About
+            </Link>
+            <Link 
+              to="/projects" 
+              className={`nav-link ${location.pathname === '/projects' ? 'active' : ''}`}
+            >
+              Projects
+            </Link>
+            <Link 
+              to="/contact" 
+              className={`nav-link ${location.pathname === '/contact' ? 'active' : ''}`}
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
