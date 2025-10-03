f030d96fe576b1a3f0b70f6a5fb0089c5b742f56
daniel.rodrigues <daniel.rodrigues@aegro.com.br>
2025-10-03 14:54:21 -0300

    Refactor import statements for consistency in About, Home, Projects components and Vite config

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './About.css';
+import { Link } from "react-router-dom";
+import "./About.css";
 
 const About = () => {
   return (
